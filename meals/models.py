9b387c856db65564c3f53cee3f9084031da0c1d8
krys27 <17830924+krys27@users.noreply.github.com>
2019-10-25 09:56:46 +0200

    improved __str__ for meals and reviews

--- meals/models.py
+++ meals/models.py
@@ -17,7 +17,7 @@ class Review(models.Model):
     text = models.CharField(max_length=256,blank=True)
 
     def __str__(self):
-        return self.text
+        return '{} wrote: \'{}\''.format(self.user.username,self.text)
 
 class Ingredient(models.Model):
     title = models.CharField(max_length=255)
--- meals/urls.py
+++ meals/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
         path('ingredients',views.show_index_empty, name='show_index_empty'),
 	path(r'ingredient/<int:pk>/', IngredientView.as_view(),name='ingredient_detail'),
 	path(r'blabla/',views.show_template),
-	##re_path(r'(P?<pk>\d+)/$', IngredientView.as_view(),name='ingredient_detail')
 	#path(r'<int:meal_id>/', views.show_index, name='show_index')
 ]
