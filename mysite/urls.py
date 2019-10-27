5ddf28ea05fc1db093631cda5db3ff9306aa5319
krys27 <17830924+krys27@users.noreply.github.com>
2019-10-27 13:59:59 +0100

    base template with sidebar

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include,path,re_path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     #path(r'meals/(\d+)/', meals.views.show_index),
     path(r'meals/', include('meals.urls')),
-]
+]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
